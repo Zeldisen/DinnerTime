85938cf48adec6e6c565725aa2e4bb0cb20d3940
Zeldisen <jeanette.norberg82@gmail.com>
2025-06-16 17:20:43 +0200

    Initial Commit

---
+++ DinnerTIme/DinnerTImeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DinnerTImeApp.swift
+//  DinnerTIme
+//
+//  Created by Jeanette Norberg on 2025-06-16.
+//
+
+import SwiftUI
+
+@main
+struct DinnerTImeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DinnerTImeTests/DinnerTImeTests.swift
@@ -0,0 +1,17 @@
+//
+//  DinnerTImeTests.swift
+//  DinnerTImeTests
+//
+//  Created by Jeanette Norberg on 2025-06-16.
+//
+
+import Testing
+@testable import DinnerTIme
+
+struct DinnerTImeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
