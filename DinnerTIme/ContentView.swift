f26f1dc690947bffb7e7872d5eb0e9ced5abe316
Zeldisen <jeanette.norberg82@gmail.com>
2025-06-16 18:11:46 +0200

    frontside done whitout logik

--- DinnerTIme/ContentView.swift
+++ DinnerTIme/ContentView.swift
@@ -10,12 +10,43 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            
+            Text("Welcome")
+                .font(.largeTitle)
+                .foregroundColor(.orange)
+                .bold()
+            Text("to")
+                .font(.largeTitle)
+                .foregroundColor(.orange)
+                .bold()
             Text("Dinner Time!")
                 .font(.largeTitle)
-                .foregroundColor(.purple)
+                .foregroundColor(.orange)
                 .bold()
             Spacer()
+            Image("front")
+                .resizable()
+                .frame(width: 250, height: 200)
+            Spacer()
+            Button("Login"){
+                
+            }
+            .padding(.bottom,10)
+            .foregroundColor(.orange)
+            .font(.system(size: 20))
+            .bold()
+            Text("Or")
+                .padding(.bottom,10)
+                .foregroundColor(.orange)
+                .font(.system(size: 20))
+                .bold()
+            Button("Sign up"){
+                
+            }
+            .foregroundColor(.orange)
+            .font(.system(size: 20))
+            .bold()
+            
+            
         }
         .padding()
     }
